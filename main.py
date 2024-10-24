a63160c87551d88d10353762b8a5162f15b90902
Danirive30 <DAniel.rivera.30@outlook.com>
2024-10-24 11:44:59 -0600

    App

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # pip install streamlit fbprophet yfinance plotly
-
 import streamlit as st
 from datetime import date
 
@@ -11,19 +10,39 @@ from plotly import graph_objs as go
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title("Stock Forecast App")
+st.title('Stock Forecast App')
 
-stocks = ("AAPL", "GOOG", "MSFT", "TSLA", "AMZN")
-selected_stocks = st.selectbox("Select dataset for prediction", stocks)
+stocks = ('GOOG', 'AAPL', 'MSFT', 'TSLA', 'AMZN')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-n_years = st.slider("Years of prediction:", 1, 5)
+n_years = st.slider('Years of prediction:', 1, 5)
 period = n_years * 365
 
 
+@st.cache_data
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
     return data
 
 
-data_load_state = st.text("Loading data...")
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'],
+                  y=data['Open'], name="stock_open"))
+    fig.add_trace(go.Scatter(x=data['Date'],
+                  y=data['Close'], name="stock_close"))
+    fig.layout.update(title_text='Time Series Data',
+                      xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+
+plot_raw_data()
